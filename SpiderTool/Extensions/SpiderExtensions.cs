e90001a9bd4061caaca9e1c9850d2b9190df1d17
sigeer <1452069418@qq.com>
2022-09-09 09:38:59 +0800

    下载优化

---
+++ SpiderTool/Extensions/SpiderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiderTool.Extensions
+{
+    public static class SpiderExtensions
+    {
+        public static string GetHostUrl(this string url)
+        {
+            var uri = new Uri(url);
+            return $"{uri.Scheme}://{uri.Host}:{uri.Port}";
+        }
+
+    }
+}
