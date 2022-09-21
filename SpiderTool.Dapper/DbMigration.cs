a32beb0b518eafc31948d303f7d205de8d08cd7a
sigeer <1452069418@qq.com>
2022-09-21 15:14:52 +0800

    dll同步

---
+++ SpiderTool.Dapper/DbMigration.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using System.Data;
+using System.Reflection;
+
+namespace SpiderTool.Dapper
+{
+    public static class DbMigration
+    {
+        public static void CreateDataBase(this IDbConnection dbConnection, string dbType)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using var sqlStream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.{dbType.ToLower()}.sql");
+
+            if (sqlStream != null)
+            {
+                using var reader = new StreamReader(sqlStream);
+                var sqlStr = reader.ReadToEnd();
+                dbConnection.Execute(sqlStr);
+            }
+        }
+    }
+}
