2f1d23df8a7715065a6fb585c0cce083738da0ee
sigeer <1452069418@qq.com>
2022-09-29 11:39:23 +0800

    重复grpc channel

---
+++ SpiderWin/Services/ServiceFactory.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Grpc.Net.Client;
+using SpiderRemoteServiceClient.Mapper.Spiders;
+using SpiderRemoteServiceClient.Services;
+using SpiderService;
+using SpiderTool.IService;
+
+namespace SpiderWin.Services
+{
+    public class ServiceFactory
+    {
+        public static GrpcChannel? GrpcChannel { get; set; }
+
+        public static void GrpcDisconnect()
+        {
+            if (GrpcChannel != null)
+            {
+                GrpcChannel.Dispose();
+                GrpcChannel = null;
+            }
+        }
+    }
+}
