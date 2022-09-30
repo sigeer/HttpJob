102d21b6aae5a474ba1083dc54524729111d2096
sigeer <1452069418@qq.com>
2022-09-30 10:24:07 +0800

    Server不再直接调用服务 通过grpc间接调用

--- SpiderRemoteServiceClient/Services/ISpiderRemoteService.cs
+++ SpiderRemoteServiceClient/Services/ISpiderRemoteService.cs
@@ -5,5 +5,8 @@ namespace SpiderRemoteServiceClient.Services
     public interface ISpiderRemoteService : ISpiderService
     {
         Task<bool> Ping();
+
+        string Crawl(int spiderId, string url);
+        Task<string> CrawlAsync(int spiderId, string url);
     }
 }
