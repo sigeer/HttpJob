291eaec22f426baa2f85f8904d231d282dea1bdb
sigeer <1452069418@qq.com>
2022-10-04 18:18:39 +0800

    增加MongoDB

---
+++ SpiderTool.Data/Mapper/SpiderProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SpiderTool.DataBase;
+using SpiderTool.Dto.Spider;
+
+namespace SpiderTool.Data.Mapper
+{
+    /// <summary>
+    /// client to server: normal dto to proto dto, proto viewmodel to normal viewmodel
+    /// </summary>
+    public class SpiderProfile : Profile
+    {
+        public SpiderProfile()
+        {
+            CreateMap<DB_Spider, SpiderDetailViewModel>();
+        }
+    }
+}
---
+++ SpiderTool.MongoDB/LibServiceProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using SpiderTool.IDomain;
+using SpiderTool.IService;
+using SpiderTool.MongoDB.Domain;
+using SpiderTool.Service;
+
+namespace SpiderTool.MongoDB
+{
+    public static class LibServiceProvider
+    {
+        public static IServiceCollection AddSpiderService(this IServiceCollection services, Func<MongoClient> options, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.Add(new ServiceDescriptor(typeof(IMongoClient), x => options(), serviceLifetime));
+
+            services.Add(new ServiceDescriptor(typeof(ISpiderDomain), typeof(SpiderDomain), serviceLifetime));
+            services.Add(new ServiceDescriptor(typeof(ITemplateDomain), typeof(TemplateDomain), serviceLifetime));
+            services.Add(new ServiceDescriptor(typeof(ITaskDomain), typeof(TaskDomain), serviceLifetime));
+
+            services.Add(new ServiceDescriptor(typeof(ISpiderService), typeof(SpiderService), serviceLifetime));
+            return services;
+        }
+    }
+}
